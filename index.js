9bd01e1b026f4fdef778c14754ac754bfca03cf6
Martin Raffáč <mraffac@gmail.com>
2022-03-22 15:14:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,6 @@ app.get('/', async(req,res) => {
     
 });
 
-app.get('/offers', async(req,res) => {
-    
-});
-
 app.get('/byty', async(req,res) => {
     
 });
@@ -35,15 +31,48 @@ app.get('/getData', async(req,res) => {
     
 });
 
-app.get('/myOffers', async(req,res) => {
-    
-});
-
 app.get('/check', async(req,res) => {
     
 });
 
-app.post('/register', async(req,res) => {
+app.post('/newOffer', async(req,res) => {
+
+});
+
+app.get('/offers', async(req,res) => {      //zobrazenie vsetkych ponuk nehnutelnosti
+
+    db.query("SELECT * FROM property INNER JOIN location ON property.location_id = location.id INNER JOIN posts ON posts.id = property.id ", (err,result) => {
+            if (err){
+                res.writeHead(400, { 'Content-Type': 'application/json' }); 
+                console.log(err)
+            }
+            else{
+                res.writeHead(200, { 'Content-Type': 'application/json' }); 
+                res.send(result);
+            }
+         }
+    );
+});
+
+app.get('/myOffers', async(req,res) => {    //najdenie vsetkych inzeratov pouzivatela
+
+    const user_id = req.body.user_id;
+
+    db.query("SELECT * FROM property INNER JOIN location ON property.location_id = location.id INNER JOIN posts ON posts.users_id = property.users_id ", (err,result) => {
+        if (err){
+            res.writeHead(400, { 'Content-Type': 'application/json' }); 
+            console.log(err)
+        }
+        else{
+            res.writeHead(200, { 'Content-Type': 'application/json' }); 
+            res.send(result);
+        }
+     }
+);
+
+});
+
+app.post('/register', async(req,res) => {       //zaregistrovanie pouzivatela
     
     const name = req.body.name;
     const surname = req.body.surname;
@@ -70,16 +99,51 @@ app.post('/register', async(req,res) => {
 
 });
 
-app.post('/newOffer', async(req,res) => {
-    
+app.put('/login', async(req,res) => {       //prihlasenie pouzivatela do aplikacie
+
+    const email = req.body.email;
+    const name = req.body.name;
+
+    db.query("SELECT * FROM users WHERE email = ? AND name = ?",
+    [email,name],
+    (err,result) => {
+        if (err){
+            res.writeHead(200, { 'Content-Type': 'application/json' }); 
+            res.send("Invalid name or email");
+            console.log(err);
+        }
+        else{
+            res.writeHead(200, { 'Content-Type': 'application/json' }); 
+            res.send(result);
+        }
+    })
 });
 
-app.put('/login', async(req,res) => {
-    
-});
+app.put('/change', async(req,res) => {      //zmenie udajov o nehnutelnosti
 
-app.get('/change', async(req,res) => {
+    const property_id = req.body.property_id;
+    const user_id = req.body.user_id;
     
+    const type = req.body.type;
+    const size = req.body.size;
+    const price = req.body.price;
+    const description = req.body.description;
+    const rooms = req.body.rooms;
+
+    db.query("UPDATE property SET (type, size, price, description, rooms) VALUES (?,?,?,?,?) WHERE id = ? AND users_id = ? ",
+    [type, size, price, description, rooms, property_id, user_id],
+    (err,result) => {
+        if (err){
+            res.writeHead(400, { 'Content-Type': 'application/json' }); 
+            res.send("Oops semething went wrong");
+            console.log(err);
+        }
+        else{
+            res.writeHead(200, { 'Content-Type': 'application/json' }); 
+            res.send(result);
+        }
+    })
+
 });
 
 app.delete('/zmaz', async(req,res) => {     // zmazanie danej nehnuteľnosti
@@ -107,4 +171,4 @@ app.delete('/zmaz', async(req,res) => {     // zmazanie danej nehnuteľnosti
 
 app.listen(8080, ()=>{                          //spustenie servera               
     console.log('Server running at port 8080');
-})
+});
