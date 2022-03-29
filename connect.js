38913934e271abf02df7a156196d579275f8be7e
8Wenders8 <matej.volansky15@gmail.com>
2022-03-29 18:57:27 +0200

    +Wrapper pre query databaz, +Async/Await, +Transakcie

--- connect.js
+++ connect.js
@@ -1,13 +1,34 @@
-const mysql = require('mysql')
+const util = require('util');
+const mysql = require('mysql');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-
+const dbConf = {
+	port: process.env.MYSQL_PORT || 3306,
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
+};
 
-});
+function getDBConnection(){
+	const connection = mysql.createConnection(dbConf);
+	return {
+		beginTransaction() {
+			return util.promisify(connection.beginTransaction).call(connection);
+		},
+		commit() {
+			return util.promisify(connection.commit).call(connection);
+		},
+		rollback() {
+			return util.promisify(connection.rollback).call(connection);
+		},
+		query(sql, args) {
+			return util.promisify(connection.query).call(connection, sql, args);
+		},
+		close() {
+			return util.promisify(connection.end).call(connection);
+		}
+	};
+}
 
-module.exports = db;
+module.exports = getDBConnection;
